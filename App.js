bf10b9b75a28279753060e3e04dc9a4c55ab098e
Tony Cui <Tonycui02@gmail.com>
2021-04-10 13:45:43 +1200

    new

--- App.js
+++ App.js
@@ -1,52 +1,75 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
-import { Container, Text } from "native-base";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Test!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
+  // const [hasPermission, setHasPermission] = useState(null);
+  // const [scanned, setScanned] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await BarCodeScanner.requestPermissionsAsync();
+  //     setHasPermission(status === "granted");
+  //   })();
+  // }, []);
 
-  const handleBarCodeScanned = ({ type, data }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    
-  };
+  // const handleBarCodeScanned = ({ type, data }) => {
+  //   setScanned(true);
+  //   alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  // };
 
-  if (hasPermission === null) {
-    return <Text>Requesting for camera permission</Text>;
-  }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
-  }
+  // if (hasPermission === null) {
+  //   return <Text>Requesting for camera permission</Text>;
+  // }
+  // if (hasPermission === false) {
+  //   return <Text>No access to camera</Text>;
+  // }
 
   return (
-    <Container>
-      {/* <View style={styles.container}> */}
-      <Content>
-        <BarCodeScanner
-          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
-          style={StyleSheet.absoluteFillObject}
-        />
-        {scanned && (
-          <Button
-            title={"Tap to Scan Again"}
-            onPress={() => setScanned(false)}
-          />
-        )}
-      </Content>
+    // <Container>
+    //   {/* <View style={styles.container}> */}
+    //   <Content>
+    //     <BarCodeScanner
+    //       onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+    //       style={StyleSheet.absoluteFillObject}
+    //     />
+    //     {scanned && (
+    //       <Button
+    //         title={"Tap to Scan Again"}
+    //         onPress={() => setScanned(false)}
+    //       />
+    //     )}
+    //   </Content>
 
-      {/* </View> */}
-    </Container>
+    //   {/* </View> */}
+    // </Container>
+
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
